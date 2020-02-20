4028f0631704019ad2dc456112f00b8460655783
Sherry <hxuan.lam4@gmail.com>
2020-02-20 15:06:56 +0700

    first commit

---
+++ client/build/precache-manifest.25e45f759f20827b69a1f28a80a60493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df352ee5272843785369ab857ecda1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ac6bacfb4b1eff6a4ca",
+    "url": "/static/css/main.d8116c63.chunk.css"
+  },
+  {
+    "revision": "36c68d140a8553eebb51",
+    "url": "/static/js/2.f738b9b9.chunk.js"
+  },
+  {
+    "revision": "2ac6bacfb4b1eff6a4ca",
+    "url": "/static/js/main.ccdb1a17.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
